c7b239b654f406c8e3a3bed4c890a3c47a38193b
test <test>
2020-10-15 10:19:57 -0400

    Create ShopPage component which takes in data for all collections.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,16 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 
 import HomePage from './pages/homePage/homePage.component';
+import ShopPage from './pages/shopPage/shopPage.component';
 
 import './App.css';
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
 function App() {
   return (
     <div >
       <Switch>
         <Route exact path="/" component={HomePage}/>
-        <Route exact path="/hats" component={HatsPage}/>
+        <Route exact path="/shop" component={ShopPage}/>
       </Switch>
     </div>
   );

608b66ad80b1ad20bb877922e98875173b496623
test <test>
2020-10-16 19:19:23 -0400

    Add header functionality to application.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homePage/homePage.component';
 import ShopPage from './pages/shopPage/shopPage.component';
-
+import Header from './components/header/header.component'
 import './App.css';
 
 function App() {
   return (
     <div >
+      <Header />
       <Switch>
-        <Route exact path="/" component={HomePage}/>
-        <Route exact path="/shop" component={ShopPage}/>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );

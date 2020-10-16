2e5f05b84a45c01ed45a2e9eeedc74900a35c657
test <test>
2020-10-16 11:59:25 -0400

    "Add collection item and list with styling."

---
+++ src/components/collectionItem/collectionItem.component.jsx
@@ -0,0 +1,20 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+import './collectionItem.style.scss';
+
+const CollectionItem = ({ id, name, price, imageUrl }) => (
+    <div className="collectionItem">
+        <div className="image"
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+        />
+        <div className="collectionFooter">
+            <span className="name">{name}</span>
+            <span className="price">{price}</span>
+        </div>
+    </div>
+)
+
+export default CollectionItem;
--- src/components/collectionPreview/collectionPreview.component.jsx
+++ src/components/collectionPreview/collectionPreview.component.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-
+import CollectionItem from '../collectionItem/collectionItem.component'
 import './collectionPreview.style.scss';
 
 const CollectionPreview = ({title, items}) => (
-    <div className="collection">
+    <div className="collectionPreview">
         <h1 className="title">{title.toUpperCase()}</h1>
         <div className="preview">
-            {items.map(item => (
-                    <div key={item.id}>{item.name}</div>
+            {items
+                .filter((item, idx) => idx < 4)
+                .map(({id, ...otherItemProps}) => (
+                    <CollectionItem key={id}{...otherItemProps}/>
             ))}
         </div>
     </div>

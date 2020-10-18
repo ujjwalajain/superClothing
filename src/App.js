c8c901e7adb9f55c619011a9d8eb76496f645af7
test <test>
2020-10-18 19:15:42 -0400

    Add firebase athentication.

--- src/App.js
+++ src/App.js
@@ -5,20 +5,46 @@ import HomePage from './pages/homePage/homePage.component';
 import ShopPage from './pages/shopPage/shopPage.component';
 import Header from './components/header/header.component'
 import Authentication from './pages/authentication/authentication.component';
+import  {auth} from './authLibrary/firebase/firebase.utils';
 
 import './App.css';
 
-function App() {
-  return (
-    <div >
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop" component={ShopPage} />
-        <Route exact path="/auth" component={Authentication} />
-      </Switch>
-    </div>
-  );
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( user => {
+      this.setState({ currentUser: user});
+
+      console.log(user);
+    } )
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div >
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/auth" component={Authentication} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
-export default App;
+

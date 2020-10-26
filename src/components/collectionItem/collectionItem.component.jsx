2196eaf4b0b86135307d7a12e02a9808a280eab1
test <test>
2020-10-26 18:35:41 -0400

    "Pass item as object so it can be used to dispatch addItems action."

--- src/components/collectionItem/collectionItem.component.jsx
+++ src/components/collectionItem/collectionItem.component.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { addItems } from '../../redux/cart/cart.actions';
 
 import CustomButton from '../controls/customButton/customButton.component';
 
 import './collectionItem.style.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl, addItemsDispatch }) => (
-    <div className="collectionItem">
-        <div className="image"
-            style={ {
-                backgroundImage: `url(${ imageUrl })`
-            } }
-        />
-        <div className="collectionFooter">
-            <span className="name">{ name }</span>
-            <span className="price">{ price }</span>
+const CollectionItem = ({ item, addItemsDispatch }) => {
+    const { name, price, imageUrl } = item
+    return (
+        <div className="collectionItem">
+            <div className="image"
+                style={ {
+                    backgroundImage: `url(${ imageUrl })`
+                } }
+            />
+            <div className="collectionFooter">
+                <span className="name">{ name }</span>
+                <span className="price">{ price }</span>
+            </div>
+            <CustomButton onClick={ () => addItemsDispatch(item) } inverted>Add To Cart</CustomButton>
         </div>
-        <CustomButton inverted>Add To Cart</CustomButton>
-    </div>
-)
+    )
+}
 
 const mapDispatchToProps = dispatch => ({
     addItemsDispatch: (items) => dispatch(items)
 })
 
-export default connect(mapDispatchToProps)(CollectionItem);
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/components/collectionPreview/collectionPreview.component.jsx
+++ src/components/collectionPreview/collectionPreview.component.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import CollectionItem from '../collectionItem/collectionItem.component'
 import './collectionPreview.style.scss';
 
-const CollectionPreview = ({title, items}) => (
+const CollectionPreview = ({ title, items }) => (
     <div className="collectionPreview">
-        <h1 className="title">{title.toUpperCase()}</h1>
+        <h1 className="title">{ title.toUpperCase() }</h1>
         <div className="preview">
-            {items
+            { items
                 .filter((item, idx) => idx < 4)
-                .map(({id, ...otherItemProps}) => (
-                    <CollectionItem key={id}{...otherItemProps}/>
-            ))}
+                .map(item => (
+                    <CollectionItem key={ item.id } item={ item } />
+                )) }
         </div>
     </div>
 )

44d9570b1ae5f6c279076328a651f6151560528c
test <test>
2020-10-29 19:01:16 -0400

    "Apply redux-persist" library to persist cart state."

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import App from './App';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 import './index.css';
 
@@ -12,7 +14,9 @@ ReactDOM.render(
   <Provider store={ store }>
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={ persistor }>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </React.StrictMode>
   </Provider>,
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,9 +1,18 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import UserReducer from './user/userReducer';
 import CartReducer from './cart/cartReducer';
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+};
+
+const rootReducer = combineReducers({
     user: UserReducer,
     cart: CartReducer
 });
+export default persistReducer(persistConfig, rootReducer);

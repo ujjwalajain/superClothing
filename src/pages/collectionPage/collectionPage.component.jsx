be0f2d538289f41b6fd833a8da1ad54d3e290761
test <test>
2020-10-30 19:59:49 -0400

    "Show items for each collection as per selected collection."

--- src/pages/collectionPage/collectionPage.component.jsx
+++ src/pages/collectionPage/collectionPage.component.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { SelectCollection } from '../../redux/shop/shop.selector';
+import CollectionItem from '../../components/collectionItem/collectionItem.component';
 
 import './collectionPage.style.scss';
 
 const CollectionPage = ({ collection }) => {
-    console.log(collection);
+    const { title, items } = collection;
     return (
         <div className="collectionPage">
-            collectionPage
+            <h2 className="title">{ title }</h2>
+            <div className="collectionItems">
+                { items.map(item => (
+                    <CollectionItem key={ item.id } item={ item } />
+                )) }
+            </div>
         </div>
     );
 };
